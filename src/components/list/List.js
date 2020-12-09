579acab615b061c936599eb46a317f37f6872e0a
Ivonaldo Soares <ivonaldojrsoares@gmail.com>
2020-12-08 22:39:49 -0300

    add styled components

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,12 +1,33 @@
 import React from "react";
 import "./List.css"
+import styled from 'styled-components';
+
+// const estilosLista = {
+//     background: 'red',
+//     color: 'white',
+//     fontSize: '16px',
+// }
+
+const ListaEstilizada = styled.ul`
+    background: red;
+    color: white;
+    font-size: 16px;
+    @media(max-width: 1000px) {
+        background: yellow;
+    }
+`;
 
 function List(props) {
     return (
         <div>
-            <ul>
+            <ListaEstilizada>
                 {props.data.map((item, index) => <li key={index}> {item} {index}</li>)}
-            </ul>
+            </ListaEstilizada>
+
+            <ListaEstilizada>
+                <li>lista 1</li>
+                <li>lista 2</li>
+            </ListaEstilizada>
             {props.children}
         </div>
     );
